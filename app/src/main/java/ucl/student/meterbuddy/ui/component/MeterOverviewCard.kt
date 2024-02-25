b2d3a7fef39818dcf11bb058aa33f6d09e4c71ab
craftbrak <louisdewilde2001@gmail.com>
2024-02-25 20:13:23 +0100

    MeterOverviewCard
    this is a first mockup

---
+++ app/src/main/java/ucl/student/meterbuddy/ui/component/MeterOverviewCard.kt
@@ -0,0 +1,88 @@
+package ucl.student.meterbuddy.ui.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ThumbUp
+import androidx.compose.material3.Card
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ElevatedSuggestionChip
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ucl.student.meterbuddy.ui.theme.MeterBuddyTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MeterOverviewCard(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {},
+    meterName: String,
+    meterIcon: ImageVector,
+    lastReading: Float,
+    readingUnit: String,
+    tendanceIcon: String,
+    tendenceValue: Float,
+    monthlyCost: Float,
+    currencySymbol: String
+) {
+    ElevatedCard(modifier = modifier, onClick = onClick) {
+        Column(modifier= Modifier
+            .fillMaxWidth(),verticalArrangement = Arrangement.SpaceAround) {
+            Row(modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceAround) {
+                Row(
+                    Modifier
+                        .padding(20.dp), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceAround){
+                    Icon(imageVector = meterIcon, contentDescription = "Meter Icon")
+                    Text(text = meterName)
+                }
+                Row(
+                    Modifier
+                        .padding(20.dp), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceEvenly){
+                    Text(text = lastReading.toString())
+                    Text(text = readingUnit)
+                }
+            }
+            Row(modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween) {
+                Row(
+                    Modifier
+                        .padding(20.dp)
+                        .fillMaxWidth(.5F), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center){
+                    Text(text = monthlyCost.toString())
+                    Text(text = currencySymbol)
+                }
+                Row(
+                    Modifier
+                        .padding(20.dp)
+                        .fillMaxWidth(.5F), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center){
+                    ElevatedSuggestionChip(onClick = { /*TODO*/ },label={
+                        Text(text = tendenceValue.toString())
+                    })
+                }
+            }
+        }
+    }
+}
+
+@Preview(name = "MeterOverviewCard")
+@Composable
+fun PreviewMeterOverviewCard() {
+    MeterBuddyTheme {
+        MeterOverviewCard(Modifier.padding(9.dp), meterName = "Electricity", meterIcon = Icons.Outlined.ThumbUp, currencySymbol = "eur", lastReading = 182.159F, monthlyCost = 291F, readingUnit = "Kwh", tendanceIcon = "", tendenceValue = 29F)
+    }
+}
