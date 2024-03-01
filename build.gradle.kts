55256b21fe53be0c630ca68d9e1e057459395e62
louis <louisdewilde2001@gmail.com>
2024-03-01 15:05:19 +0100

    Updating dependancies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.0" apply false
+    id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
 }
@@ -10,6 +10,6 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath(kotlin("gradle-plugin", version = "1.9.21"))
+        classpath(kotlin("gradle-plugin", version = "1.9.22"))
     }
 }
