0e0cb7cef38346332d01c43eeacb7e9f62bac23a
louis <louisdewilde2001@gmail.com>
2024-03-04 23:08:13 +0100

    Changing Fab to EFab with Icon

--- app/src/main/java/ucl/student/meterbuddy/ui/pages/MainPage.kt
+++ app/src/main/java/ucl/student/meterbuddy/ui/pages/MainPage.kt
@@ -5,10 +5,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -44,12 +48,15 @@ fun MainPage(mainPageScreenModel: MainPageScreenModel) {
             )
         },
         floatingActionButton ={
-            FloatingActionButton(onClick = { navigator?.push(AddMeterFormScreen(meters)) }) {
+            ExtendedFloatingActionButton(onClick = { navigator?.push(AddMeterFormScreen(meters)) }) {
+                Icon(imageVector = Icons.Outlined.Add, contentDescription = "add Meter")
                 Text("Add Meter")
             }
         }
     ) { innerPadding ->
-        LazyColumn(modifier = Modifier.fillMaxWidth().padding(innerPadding)) {
+        LazyColumn(modifier = Modifier
+            .fillMaxWidth()
+            .padding(innerPadding)) {
             items(meters) { meter ->
                 MeterOverviewCard(
                     onClick = {
