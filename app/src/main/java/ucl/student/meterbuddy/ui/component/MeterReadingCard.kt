62843ee6d25e09c97f94e1523a240e49d6f0586c
louis <louisdewilde2001@gmail.com>
2024-03-06 00:56:50 +0100

    Readings can now be Deleted from the UI

--- app/src/main/java/ucl/student/meterbuddy/ui/component/MeterReadingCard.kt
+++ app/src/main/java/ucl/student/meterbuddy/ui/component/MeterReadingCard.kt
@@ -3,15 +3,12 @@ package ucl.student.meterbuddy.ui.component
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -24,19 +21,18 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import java.util.Date
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun MeterReadingCard(modifier: Modifier,onclick : ()-> Unit, value: Float, date: LocalDateTime, note: String , onEditClick: ()-> Unit ) {
+fun MeterReadingCard(modifier: Modifier, onclick : ()-> Unit, value: Float, date: LocalDateTime, note: String, onDeleteClick: ()-> Unit ) {
     val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
     //Should be a card and need to be aligned
     Row (modifier.clickable { onclick() }.clip(shape = RoundedCornerShape(8.dp)).background(MaterialTheme.colorScheme.primaryContainer), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceAround){
         Text(text = value.toString())
         Text(text = date.format(formatter))
         Text(text = note)
-        IconButton(onClick = onEditClick) {
-            Icon(imageVector = Icons.Default.Edit, contentDescription = "edit")
+        IconButton(onClick = onDeleteClick) {
+            Icon(imageVector = Icons.Default.Delete, contentDescription = "edit")
         }
     }
 }
--- app/src/main/java/ucl/student/meterbuddy/ui/pages/MeterPage.kt
+++ app/src/main/java/ucl/student/meterbuddy/ui/pages/MeterPage.kt
@@ -1,39 +1,26 @@
 package ucl.student.meterbuddy.ui.pages
 
 import android.annotation.SuppressLint
-import android.graphics.drawable.Icon
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.Delete
-import androidx.compose.material.icons.outlined.Lock
 import androidx.compose.material.icons.rounded.ArrowBack
-import androidx.compose.material.icons.rounded.Menu
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
@@ -49,13 +36,9 @@ import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import kotlinx.coroutines.launch
-import ucl.student.meterbuddy.data.model.entity.Meter
-import ucl.student.meterbuddy.ui.component.MeterOverviewCard
 import ucl.student.meterbuddy.ui.component.MeterReadingCard
 import ucl.student.meterbuddy.ui.screen.AddReadingScreen
 import ucl.student.meterbuddy.viewmodel.MeterScreenModel
-import java.time.Instant
-import java.util.Date
 
 @RequiresApi(Build.VERSION_CODES.O)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -155,15 +138,19 @@ fun MeterPage(meterScreenModel: MeterScreenModel ) {
                     value = reading.value,
                     date = reading.date,
                     note = "id: ${reading.readingID}",
-                    onEditClick = {
-                        navigator.push(AddReadingScreen(meterScreenModel,meterScreenModel.meter.meterID,meterScreenModel.meter.meterName,reading.date,reading.value, true){ value, date, note ->
-                        meterScreenModel.updateReading(reading.readingID, value, date, note)
+                    onDeleteClick = {
+                        scope.launch {
+                            val result = snackbarHostState.showSnackbar(
+                                message = "Do you really want to delete this reading?",
+                                actionLabel = "yes",
+                                withDismissAction = true
+                            )
+                            if (result == SnackbarResult.ActionPerformed) {
+                                meterScreenModel.deleteReading(reading.readingID)
+                            }
+                        }
                     })
-                    }
-                )
             }
         }
     }
-
-
 }
