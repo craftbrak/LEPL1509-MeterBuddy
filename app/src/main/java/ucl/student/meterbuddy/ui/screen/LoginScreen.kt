23f75a5311b95a94e2d020585ec1ab5d6eb1b3d9
louis <louisdewilde2001@gmail.com>
2024-04-13 09:23:10 +0200

    Adding logo to login + estethical changes

--- app/src/main/java/ucl/student/meterbuddy/ui/screen/LoginScreen.kt
+++ app/src/main/java/ucl/student/meterbuddy/ui/screen/LoginScreen.kt
@@ -2,13 +2,18 @@ package ucl.student.meterbuddy.ui.screen
 
 import android.app.Activity
 import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,7 +24,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.hilt.getScreenModel
@@ -27,6 +34,7 @@ import cafe.adriel.voyager.hilt.getViewModel
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import ucl.student.meterbuddy.R
 import ucl.student.meterbuddy.viewmodel.MainPageScreenModel
 
 class LoginScreen : Screen {
@@ -42,23 +50,34 @@ class LoginScreen : Screen {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
         ) {
-            Text(text = "Login")
+            Spacer(modifier = Modifier.height(100.dp))
+            Image(
+                painter = painterResource(id = R.drawable.meter_budy_logo),
+                contentDescription = "App Logo",
+                modifier = Modifier
+                    .size(150.dp)
+                    .clip(CircleShape)
+            )
+            Spacer(modifier = Modifier.height(50.dp))
+            Text(text = "Login", style = MaterialTheme.typography.headlineLarge)
+            Spacer(modifier = Modifier.height(25.dp))
             OutlinedTextField(
                 value = email,
                 onValueChange = { email = it },
                 label = { Text("Email") },
                 modifier = Modifier.fillMaxWidth()
             )
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(20.dp))
             OutlinedTextField(
                 value = password,
                 onValueChange = { password = it },
                 label = { Text("Password") },
                 modifier = Modifier.fillMaxWidth()
             )
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(50.dp))
             Button(onClick = { mainPageScreenModel.loginUser(email, password) }) {
                 Text("Login")
             }
