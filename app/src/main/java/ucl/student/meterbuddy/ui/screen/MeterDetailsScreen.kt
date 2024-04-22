d5fa61c7a0de7c02f93cf59e23fa13fc952420c2
Mathis Delsart <mathis.delsart@student.uclouvain.be>
2024-04-22 22:24:06 +0200

    Add Indication Button To Add First Reading

--- app/src/main/java/ucl/student/meterbuddy/ui/screen/MeterDetailsScreen.kt
+++ app/src/main/java/ucl/student/meterbuddy/ui/screen/MeterDetailsScreen.kt
@@ -1,17 +1,23 @@
 package ucl.student.meterbuddy.ui.screen
 
+import android.database.Cursor
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
 import androidx.compose.material3.ElevatedSuggestionChip
@@ -28,13 +34,18 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerHoverIcon
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.hilt.getScreenModel
@@ -60,37 +71,41 @@ data class MeterDetailsScreen(val meter: Meter): Screen {
         val meterScreenModel = getScreenModel<MeterScreenModel,MeterScreenModel.Factory>{
             it.create(meter)
         }
-
         Scaffold(
             modifier = Modifier.fillMaxSize(),
             snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
             topBar = { AppTopBar(meter.meterName, navigator, scope, snackbarHostState) },
             floatingActionButton = { AdderButton(meterScreenModel, navigator) },
-            bottomBar = { BottomAppBar {
-
-            }}
+            bottomBar = { BottomAppBar { } }
         ) { innerPadding ->
             LazyColumn(modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)) {
                 item { BoxGraph(meterScreenModel) }
                 item { Text(text = "Meter Readings", style = MaterialTheme.typography.headlineSmall, modifier = Modifier.padding(8.dp)) }
-                items(meterScreenModel.state.value.readings) { reading ->
-                    MeterReadingCard(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp),
-                        onclick =
-                        {
-                            navigator.push(AddReadingScreen(meterScreenModel.meter.meterName, reading.date, reading.value,reading.note, true) { value, date, note ->
-                                meterScreenModel.updateReading(reading.readingID, value, date, note)
-                            })
-                        },
-                        value = reading.value,
-                        date = reading.date,
-                        note = reading.note,
-                        onDeleteClick = { deleteReadingButton(reading.readingID, meterScreenModel, snackbarHostState, scope) }
-                    )
+                if (meterScreenModel.state.value.readings.isEmpty()) {
+                    // item { Text(text = "Create your first reading just here !") }
+                    item { AddFirstReadingIndicator(meterScreenModel, navigator)
+                    }
+                }
+                else {
+                    items(meterScreenModel.state.value.readings) { reading ->
+                        MeterReadingCard(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp),
+                            onclick =
+                            {
+                                navigator.push(AddReadingScreen(meterScreenModel.meter.meterName, reading.date, reading.value,reading.note, true) { value, date, note ->
+                                    meterScreenModel.updateReading(reading.readingID, value, date, note)
+                                })
+                            },
+                            value = reading.value,
+                            date = reading.date,
+                            note = reading.note,
+                            onDeleteClick = { deleteReadingButton(reading.readingID, meterScreenModel, snackbarHostState, scope) }
+                        )
+                    }
                 }
             }
         }
@@ -128,19 +143,73 @@ data class MeterDetailsScreen(val meter: Meter): Screen {
     }
 
     @Composable
-    private fun AdderButton(meterScreenModel: MeterScreenModel, navigator: Navigator) {
-        FloatingActionButton(
-            // Handle the click
-            onClick = { },
-            containerColor = MaterialTheme.colorScheme.secondary,
-            shape = MaterialTheme.shapes.extraLarge
+    private fun AddFirstReadingIndicator(meterScreenModel: MeterScreenModel, navigator: Navigator) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth()
         ) {
-            IconButton(onClick = {
-                navigator.push(AddReadingScreen(meterScreenModel.meter.meterName) { value, date, note ->
-                    meterScreenModel.addReading(value, date, note)
-                })
-            }, modifier = Modifier) {
-                Icon(imageVector = Icons.Outlined.Add, contentDescription = "Sort")
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 32.dp)
+                    .background(color = MaterialTheme.colorScheme.secondaryContainer),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(
+                        text = "Add your first reading",
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.secondary,
+                        modifier = Modifier.padding(bottom = 8.dp)
+                    )
+                    Text(
+                        text = "Capture and track your first reading to start monitoring your usage.",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(bottom = 8.dp)
+                    )
+                }
+            }
+            FloatingActionButton(
+                onClick = {
+                    navigator.push(AddReadingScreen(meterScreenModel.meter.meterName) { value, date, note ->
+                        meterScreenModel.addReading(value, date, note)
+                    })
+                },
+                containerColor = MaterialTheme.colorScheme.secondary,
+                modifier = Modifier
+                    .size(56.dp)
+                    .align(Alignment.CenterHorizontally),
+                shape = CircleShape,
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Add,
+                    contentDescription = "Add Reading",
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+        }
+    }
+
+    @Composable
+    private fun AdderButton(meterScreenModel: MeterScreenModel, navigator: Navigator) {
+        if (meterScreenModel.state.value.readings.isNotEmpty()) {
+            FloatingActionButton(
+                onClick = { },
+                containerColor = MaterialTheme.colorScheme.secondary,
+                shape = MaterialTheme.shapes.extraLarge
+            ) {
+                IconButton(onClick = {
+                    navigator.push(AddReadingScreen(meterScreenModel.meter.meterName) { value, date, note ->
+                        meterScreenModel.addReading(value, date, note)
+                    })
+                }, modifier = Modifier) {
+                    Icon(imageVector = Icons.Outlined.Add, contentDescription = "Sort")
+                }
             }
         }
     }
@@ -148,7 +217,8 @@ data class MeterDetailsScreen(val meter: Meter): Screen {
     @Composable
     private fun BoxGraph(meterScreenModel: MeterScreenModel) {
         Box(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
                 .fillMaxSize()
                 .background(Color.White),
             contentAlignment = Alignment.Center
@@ -159,8 +229,7 @@ data class MeterDetailsScreen(val meter: Meter): Screen {
                     modifier = Modifier.padding(10.dp)
                 ) {
                     val readings = meterScreenModel.state.value.readings
-                    if (readings.size >= 2)
-                    {
+                    if (readings.size >= 2) {
                         val graph = ChartLineModel.createChartLine(
                                         readings = readings,
                                         type = meterScreenModel.meter.meterType,
@@ -170,15 +239,11 @@ data class MeterDetailsScreen(val meter: Meter): Screen {
                                                         width = LocalConfiguration.current.screenWidthDp,
                                                         height = 300
                         )
-                    }
-                    else
-                    {
-                        Image( painter = painterResource(id = R.drawable.data_pending),
+                    } else { Image( painter = painterResource(id = R.drawable.data_pending),
                             contentDescription = "Data Pending",
                             modifier = Modifier
                                 .align(Alignment.Center)
-                        )
-                    }
+                    )}
                 }
             }
         }
