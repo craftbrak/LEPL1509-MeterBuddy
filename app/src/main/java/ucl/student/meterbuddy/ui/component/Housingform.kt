e82f216b94a4463597d3d10b0c8f6f9bfe6411f9
louis <louisdewilde2001@gmail.com>
2024-05-01 15:47:59 +0200

    Adding tooltip to HousingFrom

--- app/src/main/java/ucl/student/meterbuddy/ui/component/Housingform.kt
+++ app/src/main/java/ucl/student/meterbuddy/ui/component/Housingform.kt
@@ -1,6 +1,7 @@
 package ucl.student.meterbuddy.ui.component
 
 import android.util.Log
+import androidx.compose.foundation.MutatePriority
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
@@ -28,15 +29,20 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.InputChip
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.SegmentedButton
 import androidx.compose.material3.SegmentedButtonDefaults
 import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.TooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -48,6 +54,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import co.yml.charts.common.extensions.isNotNull
+import kotlinx.coroutines.launch
 import ucl.student.meterbuddy.data.model.entity.Housing
 import ucl.student.meterbuddy.data.model.entity.User
 import ucl.student.meterbuddy.data.model.enums.HousingType
@@ -178,27 +185,52 @@ fun HousingFrom(
                 Card (Modifier.padding(15.dp)) {
                     Column (modifier= Modifier.padding(15.dp),horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center){
                         Text("Add Member To $housingName")
-                        OutlinedTextField(
-                            userSearch,
-                            { userSearch = it },
-                            label = { Text("Search") },
-                            placeholder = { Text("username") },
-                            leadingIcon = {
-                                Icon(
-                                    imageVector = Icons.Filled.Search,
-                                    contentDescription = Icons.Filled.Search.name
-                                )
-                            },
-                            trailingIcon = {
-                                IconButton(onClick = { userSearch = "" }) {
-                                    Icon(
-                                        imageVector = Icons.Filled.Close,
-                                        contentDescription = Icons.Filled.Close.name
-                                    )
+                        val tooltipState = remember { mutableStateOf(TooltipState()) }
+                        val scope = rememberCoroutineScope()
+                        TooltipBox(
+                            positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
+                            tooltip = {
+                                PlainTooltip {
+                                    Text("Search for a user to add to the housing")
                                 }
                             },
-                            singleLine = true
-                        )
+                            state = tooltipState.value,
+                            content = {
+                                OutlinedTextField(
+                                    userSearch,
+                                    { userSearch = it },
+                                    label = { Text("Search") },
+                                    placeholder = { Text("username") },
+                                    leadingIcon = {
+                                        IconButton(
+                                            onClick = {
+                                                scope.launch {
+                                                    tooltipState.value.show(
+                                                        MutatePriority.Default
+                                                    )
+                                                }
+                                            },
+                                        ) {
+                                            Icon(
+                                                imageVector = Icons.Filled.Search,
+                                                contentDescription = Icons.Filled.Search.name
+                                            )
+                                        }
+
+                                    },
+                                    trailingIcon = {
+                                        IconButton(onClick = { userSearch = "" }) {
+                                            Icon(
+                                                imageVector = Icons.Filled.Close,
+                                                contentDescription = Icons.Filled.Close.name
+                                            )
+                                        }
+                                    },
+                                    singleLine = true
+                                )
+
+
+                            })
                         FlowRow(modifier = Modifier.heightIn(50.dp,500.dp)) {
                             users.filter { u ->
                                 u.userName.contains(
