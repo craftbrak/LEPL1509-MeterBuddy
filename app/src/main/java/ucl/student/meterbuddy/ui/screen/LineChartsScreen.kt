64b4e4c86ba3f3a9befd5ec2c8f39819d440e77e
Mathis Delsart <mathis.delsart@student.uclouvain.be>
2024-05-01 10:10:27 +0200

    AJout ecran graph

--- app/src/main/java/ucl/student/meterbuddy/ui/screen/LineChartsScreen.kt
+++ app/src/main/java/ucl/student/meterbuddy/ui/screen/LineChartsScreen.kt
@@ -1,39 +1,58 @@
 package ucl.student.meterbuddy.ui.screen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.hilt.getViewModel
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
 import co.yml.charts.ui.linechart.model.LineChartData
 import ucl.student.meterbuddy.R
+import ucl.student.meterbuddy.data.model.entity.Housing
 import ucl.student.meterbuddy.data.model.entity.Meter
 import ucl.student.meterbuddy.data.model.entity.MeterReading
 import ucl.student.meterbuddy.data.model.enums.MeterType
 import ucl.student.meterbuddy.data.model.enums.MeterUnit
+import ucl.student.meterbuddy.data.utils.DataException
+import ucl.student.meterbuddy.data.utils.Resource
 import ucl.student.meterbuddy.viewmodel.ChartLineModel
 import ucl.student.meterbuddy.viewmodel.MainPageScreenModel
 import kotlin.math.abs
@@ -83,9 +102,7 @@ class LineChartsScreen: Tab {
                 var idxFirstReadings_production = 0
                 var idxFirstReadings_consumption = 0
                 metersFiltered.forEach { meter ->
-                    val readingsNotFiltered =
-                        mainPageScreenModel.state.value.lastReading[meter.meterID]
-                            ?: emptyList()
+                    val readingsNotFiltered = mainPageScreenModel.state.value.lastReading[meter.meterID] ?: emptyList()
 
                     if (meter.additiveMeter) {
                         readings_consumption += mainPageScreenModel.convertUnitReadings(
@@ -175,13 +192,11 @@ class LineChartsScreen: Tab {
             }
         }
 
-        if (listMeterTab.isEmpty()) { Text("You need at least one meter with two readings.")  }
+        if (listMeterTab.isEmpty()) { firstIndicationGraphScreen() }
         else
         {
             Scaffold(
-                bottomBar = {
-                    BottomAppBar { }
-                }
+                bottomBar = { BottomAppBar { } }
             ) { innerPadding ->
                 Column(horizontalAlignment = Alignment.CenterHorizontally) {
                     Text(text = "Total Energy Consumption by category", fontWeight = FontWeight.Bold)
@@ -204,7 +219,108 @@ class LineChartsScreen: Tab {
     }
 
     @Composable
-    fun GraphBox(param : MeterTab){
+    private fun firstIndicationGraphScreen() {
+        Scaffold(
+            bottomBar = { BottomAppBar { } },
+        ) { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "Welcome to the realm of visualization!", fontWeight = FontWeight.Bold)
+                HorizontalDivider(Modifier.padding(20.dp))
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(
+                        text = "Get ready to visualize your energy and water usage like never before!",
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+                }
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Info,
+                        contentDescription = "Info",
+                        tint = MaterialTheme.colorScheme.secondary,
+                        modifier = Modifier.size(16.dp).alignByBaseline()
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(
+                        text = "Our app intelligently groups your meters by category (electricity, water, etc.), merging those of the same type.",
+                        style = MaterialTheme.typography.bodyMedium,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+                }
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Info,
+                        contentDescription = "Info",
+                        tint = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier.size(16.dp).alignByBaseline()
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(
+                        text = "We distinguish between production lines in green and consumption lines in red on our graphs.",
+                        style = MaterialTheme.typography.bodyMedium,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+                }
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Info,
+                        contentDescription = "Info",
+                        tint = MaterialTheme.colorScheme.secondary,
+                        modifier = Modifier.size(16.dp).alignByBaseline()
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(
+                        text = "You'll also see your total consumption/production and associated cost, calculated considering individual costs before merging similar meters.",
+                        style = MaterialTheme.typography.bodyMedium,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+                }
+
+                HorizontalDivider(Modifier.padding(20.dp))
+
+                Text(
+                    text = "There is no data available for visualization yet. Add at least two readings to one of your meters to start visualizing your energy consumption!",
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.primary,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+            }
+        }
+    }
+
+
+    @Composable
+    private fun GraphBox(param : MeterTab){
         Column(
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
